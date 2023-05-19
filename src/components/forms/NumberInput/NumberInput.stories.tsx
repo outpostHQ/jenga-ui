9fc4f1d29386a55fa51ff36a5c5a4265333c9e6e
taranbeer <staranbeer@gmail.com>
2023-05-19 16:53:56 +0530

    updated the numberInput Stories

--- src/components/forms/NumberInput/NumberInput.stories.tsx
+++ src/components/forms/NumberInput/NumberInput.stories.tsx
@@ -1,4 +1,5 @@
 import { CurrencyCircleDollar } from '@jengaicons/react';
+import { StoryFn } from '@storybook/react';
 
 import { baseProps } from '../../../stories/lists/baseProps';
 import { NUMBER_VALUE_ARG } from '../../../stories/FormFieldArgs';
@@ -18,7 +19,7 @@ export default {
   },
 };
 
-const Template = ({ icon, ...props }) => (
+const Template: StoryFn<typeof NumberInput> = ({ icon, ...props }) => (
   <NumberInput
     prefix={icon ? <CurrencyCircleDollar /> : null}
     {...props}
@@ -31,11 +32,6 @@ Default.args = {};
 
 export const WithDefaultValue = Template.bind({});
 WithDefaultValue.args = { defaultValue: 5 };
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-};
 
 export const Disabled = Template.bind({});
 Disabled.args = {
